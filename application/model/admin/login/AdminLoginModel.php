00ce157b0537a82d5e9b6fcddeb4fc1bbfea1325
Jonas971 <jonasxtrm971@gmail.com>
2020-02-19 13:44:37 +0100

    hinting et nettoyage de AdmindLoginModel.php

--- application/model/admin/login/AdminLoginModel.php
+++ application/model/admin/login/AdminLoginModel.php
@@ -1,22 +1,18 @@
 <?php
-//model , gestion de la base de donnée
-
-//inclure la bdd
 require_once 'config/DataBase.php';
 
 
 /** login  /CONNEXION 
+ * @param string $email
+ * @param string $password
  * 
- * @param string
- * 
- * @return void/array
+ * @return void|array
 */
-function loginAdmin($email, $password){
-    //connexion à la bdd
+function loginAdmin(string $email, string $password) : ?array
+{
     $db = new Database;
     $db = $db->dbConnect();
 
-    //si le mail est dans la table boerbull_admin , on selectionne tous le contenue de cette table
     $sql = "SELECT * From boerbull_admin WHERE mail = :mail ";
 
     $adminExist = $db->prepare($sql);
@@ -24,35 +20,27 @@ function loginAdmin($email, $password){
     $adminExist = $adminExist->fetch();
 
 
-    //on teste si le mail exist dans la table user
     if(empty($adminExist)){
         throw new PDOException(('admin inconnu - cet email n\' existe pas'));
     }
 
-    //on teste si le mot de passe correspond a celui qui est dans la bdd
     if(!password_verify($password, $adminExist['password'])){
         throw new PDOException('Le mot de passe est incorrect');
     } 
-
     return $adminExist;
-    
 }            
 
 
-
-
 /** on récupère tous les admins 
- * 
  * @return array
 */
-function adminAll(){
-    //connexion à la bdd
+function adminAll() : array
+{
     $db = new Database;
     $db = $db->dbConnect();
 
     $sql = 'SELECT * FROM boerbull_admin';
     $adminAll = $db->query($sql);
     $adminAll = $adminAll->fetchAll();
-
     return $adminAll;
 }
