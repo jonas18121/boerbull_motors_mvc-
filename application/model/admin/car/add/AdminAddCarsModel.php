8f8b1072c3e70a80479afd60d31c3bce014c2893
Jonas971 <jonasxtrm971@gmail.com>
2020-02-25 09:55:38 +0100

    nettoyage et hiting AdminAddCarsModel.php et AdminDeleteCarsModel.php

--- application/model/admin/car/add/AdminAddCarsModel.php
+++ application/model/admin/car/add/AdminAddCarsModel.php
@@ -1,17 +1,24 @@
 <?php
-//model , gestion de la base de donnée
-
-//inclure la bdd
 require_once 'config/DataBase.php';
 
 /** admin ajoute une voiture 
  *
- * @param string/int
- * 
+ * @param string $marque
+ * @param string $modele
+ * @param string $color
+ * @param string $moteur
+ * @param string $carburant
+ * @param string $image_url
+ * @param int $annee
+ * @param int $conso
+ * @param int $prix_trois_jours
+ * @param int $puissance
+ * @param int $id_category
+ * @param int $cent
  * @return void  
  */ 
-function addCars($marque, $modele, $annee, $conso, $color, $prix_trois_jours, $puissance, $moteur, $carburant, $cent, $nombre_de_place, $id_category, $image_url){
-
+function addCars(string $marque, string $modele, int $annee, int $conso, string $color, int $prix_trois_jours, int $puissance, string $moteur, string $carburant, int $cent, int $nombre_de_place, int $id_category, string $image_url) :void
+{
     $db = new Database;
     $db = $db->dbConnect();
      
@@ -19,36 +26,34 @@ function addCars($marque, $modele, $annee, $conso, $color, $prix_trois_jours, $p
     VALUES(:marque, :modele, :annee, :conso, :color, :prix_trois_jours, :puissance, :moteur, :carburant, :cent, :nombre_de_place, :id_category, :image_url)";
 
 
-        $addCar = $db->prepare($sql);
+    $addCar = $db->prepare($sql);
         
-        $addCar->execute([
+    $addCar->execute([
 
-            ':marque' => $marque, 
-            ':modele' => $modele, 
-            ':annee' => $annee,
-            ':conso' => $conso,
-            ':color' => $color,
-            ':prix_trois_jours' => $prix_trois_jours,
-            ':puissance' => $puissance,
-            ':moteur' => $moteur,
-            ':carburant' => $carburant,
-            ':cent' => $cent,
-            ':nombre_de_place' => $nombre_de_place,
-            ':id_category' => $id_category, 
-            ':image_url' => $image_url
-        ]);
-        
-
+        ':marque' => $marque, 
+        ':modele' => $modele, 
+        ':annee' => $annee,
+        ':conso' => $conso,
+        ':color' => $color,
+        ':prix_trois_jours' => $prix_trois_jours,
+        ':puissance' => $puissance,
+        ':moteur' => $moteur,
+        ':carburant' => $carburant,
+        ':cent' => $cent,
+        ':nombre_de_place' => $nombre_de_place,
+        ':id_category' => $id_category, 
+        ':image_url' => $image_url
+    ]);
 }
 
 
 
 /** admin ajoute une categorie à la voiture qu'on a ajouter 
  * 
- * @return array
+ * @return array $category
 */
-function category(){
-
+function category() : array
+{
     $db = new Database;
     $db = $db->dbConnect();
 
