9679155cd9dca5569a80f7aa31cf5ad93b7d08e9
Jonas971 <jonasxtrm971@gmail.com>
2020-03-03 11:09:16 +0100

    nettoyage fichier homeModel.php

--- application/controller/HomeController.php
+++ application/controller/HomeController.php
@@ -1,15 +1,11 @@
 <?php
-//controlleur, mettre en relation le model et la vue 
-
-//appel du model
 require_once 'model/HomeModel.php';
 
-//A partir du routeur , getHome() appelera notre function findHome()
-function getHome(){
-
-    //appel de la fontion du model
+/**
+ * Appel le model HomeModel.php
+ */
+function getHome() : void
+{
     $home = findHome();
-
-    //appel de la vue
     require_once 'www/templates/HomeView.phtml';
 }
