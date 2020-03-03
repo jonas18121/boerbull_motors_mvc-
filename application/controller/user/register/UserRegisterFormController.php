cbf8e2c1d3de0f33ecc80c9c5a1bc5a0fc82fdc9
Jonas971 <jonasxtrm971@gmail.com>
2020-03-03 11:27:56 +0100

    nettoyage UserRegisterFormModel.php

--- application/controller/user/register/UserRegisterFormController.php
+++ application/controller/user/register/UserRegisterFormController.php
@@ -1,24 +1,23 @@
 <?php
-//controlleur mettre en relation le model et la vue 
-
-//appel du model
 require_once 'model/user/register/UserRegisterFormModel.php';
-
-
-//appel d'un fichier dans la librairie
 require_once 'library/Tools.php';
 
 //en $_GET
-function userRegisterForm(){
-    //appel de la vue
+/**
+ * Appel la vue UserRegisterFormView.phtml pour afficher le formulaire
+ */
+function userRegisterForm()
+{
     require_once 'www/templates/user/register/UserRegisterFormView.phtml';
 }
 
 //$_POST
-//A partir du routeur , il appelera notre function userRegisterFormOk()
-function userRegister(){
-
-    //controle de formulaire en php
+/**
+ *  Controle les données envoyer par l'utilistateur et appel le model UserRegisterFormModel pour enregistrer les données
+ * si elles sont bonnes 
+ */
+function userRegister()
+{
     if(!empty($_POST)){
         if(array_key_exists('first_name',$_POST) && isset($_POST['first_name']) && ctype_alpha($_POST['first_name'])){
             if( strlen($_POST['first_name']) >= 2 && strlen($_POST['first_name']) <= 40){
@@ -27,8 +26,6 @@ function userRegister(){
                         if(array_key_exists('password',$_POST) && isset($_POST['password']) && strlen($_POST['password']) >= 8){
                             if(array_key_exists('mail',$_POST) && isset($_POST['mail'])){
                                 if(preg_match("/^[a-zA-Z][a-zA-Z0-9._-]{1,19}@[a-z]{4,7}\.[a-z]{2,3}$/", $_POST['mail'])){
-
-                                    //si tous les controles sont bon
                                     //inscription user
                                     registerUser((string)$_POST['first_name'] , (string)$_POST['last_name'], (string)$_POST['mail'], (string)$_POST['password']);
 
@@ -42,8 +39,6 @@ function userRegister(){
             }
         } 
     }
-    
-  
     // s'il y a un controle qui n'est pas bon, on redirectionne à la page d' inscription pour user
     redirect('index.php?action=user&action2=registerForm');
 }
