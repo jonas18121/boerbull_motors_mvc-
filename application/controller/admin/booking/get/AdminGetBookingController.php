436ca8eb00ba5e8add49a2b73f5eac0a520e98ea
Jonas971 <jonasxtrm971@gmail.com>
2020-03-12 09:35:10 +0100

    netteoyage

--- application/controller/admin/booking/get/AdminGetBookingController.php
+++ application/controller/admin/booking/get/AdminGetBookingController.php
@@ -1,27 +1,18 @@
 <?php
-//controlleur mettre en relation le model et la vue 
-
-//appel du model
 require_once 'model/admin/booking/get/AdminGetBookingModel.php';
-
-//appel de la session
 require_once 'aSession/AdminSession.php';
-
-//appel d'un fichier dans la librairie
 require_once 'library/Tools.php';
 
 
 //en GET
-function adminGetBooking(){
-
-    //si le admin n'est pas connecter au le renvois a l'accueil
+/** afficher les RDV
+ * @return void
+ */
+function adminGetBooking() : void
+{
     if(!isAuthenticatedAdmin()){
         redirect("index.php");
     }
-    
-    //appel de la fontion du model
     $adminFindBooking = findBooking();
-
-    //appel de la vue
     require_once 'www/templates/admin/booking/get/AdminGetBookingView.phtml';
 } 
