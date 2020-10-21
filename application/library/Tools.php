7e5988b746b363a87232c1cb4df5d0f8fbca43ff
jonas18121 <jonasxtrm971@gmail.com>
2020-10-21 16:42:48 +0200

    ajout de pagination pour le nombre de voiture à affiché dans une page, par catégories

--- application/library/Tools.php
+++ application/library/Tools.php
@@ -12,24 +12,39 @@ function redirect($url){
 // boite à outils php 
 
 /**
- * var_dump avec <pre></pre> pour le debogage de code
+ * var_dump avec < pre >< / pre > pour le debogage de code
  * 
  * @param mixed $param1 - Tous les types de param sont accepter
- * @param mixed $param1 - (facultative) Tous les types de param sont accepter
+ * @param mixed $param2 - (facultative) Tous les types de param sont accepter
+ * @param bool $param3  - activer ou pas, la function die()
  * 
  * @return void - retourn la valeur de var_dump
  */
-function pre_var_dump($param1, $param2 = null) : void
+function pre_var_dump($param1, $param2 = null, $param3 = false) : void
 {
-    if ($param2 === null) {
-        echo '<pre>';
-        var_dump($param1);
-        echo '</pre>';
-    }
-    else{
-        echo '<pre>';
-        var_dump($param1, $param2);
-        echo '</pre>';
+    if ($param3 === false) {
+        
+        if ($param2 === null) {
+            echo '<pre>';
+            var_dump($param1);
+            echo '</pre>';
+        }
+        else{
+            echo '<pre>';
+            var_dump($param1, $param2);
+            echo '</pre>';
+        }
+    }else{
+        if ($param2 === null) {
+            echo '<pre>';
+            var_dump($param1);die;
+            echo '</pre>';
+        }
+        else{
+            echo '<pre>';
+            var_dump($param1, $param2);die;
+            echo '</pre>';
+        }
     }
 }
 
--- application/model/category/CategoryModel.php
+++ application/model/category/CategoryModel.php
@@ -2,18 +2,24 @@
 require_once 'config/DataBase.php';
 require_once 'library/Tools.php';
 
-/** selectionner par categorie 
+/** selectionner des voitures par categorie 
  * 
  * @param int $category
  * @return array $categories
 */
-function findCategory(int $category) :array
+function findCategory(int $category, int $premier, int $nb_car_par_page) : array
 {
     //connexion à la bdd
     $db = new Database;
     $db = $db->dbConnect();
 
-    $sql = "SELECT car.id, image_url, modele, marque, name FROM car INNER JOIN category ON category.id = car.id_category WHERE category.id = :id_category";
+    $sql = "SELECT car.id, image_url, modele, marque, name 
+        FROM car 
+        INNER JOIN category 
+        ON category.id = car.id_category 
+        WHERE category.id = :id_category
+        LIMIT {$premier}, {$nb_car_par_page}"
+    ;
 
     $categories = $db->prepare($sql);
     $categories->execute(array('id_category' => $category));
@@ -43,5 +49,25 @@ function findAllo() :array
     $categories = $db->query($sql);
     $categories = $categories->fetchAll();
     return  $categories;
+}
+
+/**
+ * compter le nombre de voiture dans une categorie
+ */
+function nb_car(int $category)  : string
+{
+    $db = new Database;
+    $db = $db->dbConnect();
+    
+    $sql = "SELECT COUNT(*) AS nb_car FROM category, car WHERE category.id = {$category} AND car.id_category = category.id";
+
+    $nb_car = $db->query($sql);
+    $nb_car = $nb_car->fetch();
+
+    if(empty($nb_car)){
+        redirect("index.php");
+    }
+    // pre_var_dump($nb_car['nb_car'], null, true);
+    return (int) $nb_car['nb_car'];
 }
 
