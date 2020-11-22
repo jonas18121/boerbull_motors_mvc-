a11e8795af1dc3406cb2cd6802254c35cfdbc327
jonas18121 <jonasxtrm971@gmail.com>
2020-11-22 19:20:01 +0100

    upload de fichier dans admin addcar

--- application/controller/admin/car/delete/AdminDeleteCarsController.php
+++ application/controller/admin/car/delete/AdminDeleteCarsController.php
@@ -1,13 +1,8 @@
 <?php
 //controlleur mettre en relation le model et la vue 
 
-//appel du model
 require_once 'model/admin/car/delete/AdminDeleteCarsModel.php';
-
-//appel de la session
 require_once 'aSession/AdminSession.php';
-
-//appel du fichier dans la librairie
 require_once 'library/Tools.php';
 
 
@@ -19,10 +14,11 @@ function adminDeleteCars(){
     if(!isAuthenticatedAdmin()){
         redirect("index.php");
     }
-
-    // Avec $_GET, on recupère la valeur de l'id qui est dans l'url 
+    
+    // Avec $_GET, on recupère la valeur de l'id et le nom de l'image qui sont dans l'url 
     deleteCar((int)$_GET['id']);
-
+    unlink("www/imgBoerbullMotors/" . $_GET['image']);
+    
     //on redirectionne l'admin vers la liste des voitures
     redirect("index.php?action=admin&action2=car&action3=get");
 }
